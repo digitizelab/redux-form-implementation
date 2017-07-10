70a9fa0cdfeae9e962cb2ff5624281abd5c02553
Nishan K <nishan@digitizelab.com>
2017-07-10 22:00:33 +0530

    Delete post implementation

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,14 +2,23 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
-import {fetchPost} from '../actions';
+import {fetchPost, deletePost} from '../actions';
 
 class PostShow extends Component {
     componentDidMount() {
+        //Caching possibility
+        //if(!this.props.post){}
         //Router method to access params
         const {id} = this.props.match.params;
         this.props.fetchPost(id);
     }
+
+    onDeleteClick = () => {
+        const {id} = this.props.match.params;
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
+    };
 
     render() {
         const {post} = this.props;
@@ -21,6 +30,12 @@ class PostShow extends Component {
         return (
             <div>
                 <Link to="/" className="btn btn-primary">Back to Index</Link>
+                <button
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick}
+                >
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -36,4 +51,4 @@ function mapStateToProps({posts}, ownProps) {
     return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostShow);
